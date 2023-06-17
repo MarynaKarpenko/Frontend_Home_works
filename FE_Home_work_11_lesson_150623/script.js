5c4239ad5abcec2660c05a21c450805f2c8fe62b
marina <mariina.karpenko@gmail.com>
2023-06-17 14:54:45 +0200

    change

--- FE_Home_work_11_lesson_150623/script.js
+++ FE_Home_work_11_lesson_150623/script.js
@@ -6,24 +6,28 @@ let buttons = document.querySelectorAll("button");
 for (let i = 0; i < buttons.length; i++) {
     if (buttons[i].classList.contains('red-btn')) {
         buttons[i].addEventListener("click", function () {
+            document.body.style.backgroundImage = "none"
             document.body.style.backgroundColor = "#F08080"
             buttons[i].style.backgroundColor = "red"
             buttons[i].style.color = "black"
         });
     } else if (buttons[i].classList.contains('green-btn')) {
         buttons[i].addEventListener("click", function () {
+            document.body.style.backgroundImage = "none"
             document.body.style.backgroundColor = "#00FF7F"
             buttons[i].style.backgroundColor = "green"
             buttons[i].style.color = "black"
         });
     } else if (buttons[i].classList.contains('blue-btn')) {
         buttons[i].addEventListener("click", function () {
+            document.body.style.backgroundImage = "none"
             document.body.style.backgroundColor = "#00CED1"
             buttons[i].style.backgroundColor = "blue"
             buttons[i].style.color = "black"
         });
     } else if (buttons[i].classList.contains('yellow-btn')) {
         buttons[i].addEventListener("click", function () {
+            document.body.style.backgroundImage = "none"
             document.body.style.backgroundColor = "#FFEFD5"
             buttons[i].style.backgroundColor = "yellow"
             buttons[i].style.color = "black"
